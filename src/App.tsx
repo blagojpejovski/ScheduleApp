db829515bc75eb92f691aad2e23bdbb5b7b16f64
Blagoj Pejovski <blagoj.pejovski@hotmail.com>
2023-09-21 22:10:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import AppBackground from './components/atoms/AppBackground';
+
+
+function App() {
+  return (
+   <AppBackground/>
+  )
+}
+
+export default App;
---
+++ src/components/atoms/AppBackground.tsx
@@ -0,0 +1,11 @@
+import BgImage from "../../assets/images/business-meeting.png"
+import "./AppBackground.css"
+const AppBackground = () => { 
+    return ( 
+        <div className="app-container">
+                <img  src={BgImage} className="top-image" alt="err"/>
+        </div>
+    )
+}
+
+export default AppBackground
